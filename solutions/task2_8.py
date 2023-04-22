be1dcf602533fbec5ed0ee97015e710d19b8e6de
NoOsip <106018325+NoOsip@users.noreply.github.com>
2023-04-22 11:46:17 +0500

    Update task2_8.py
    It work!

--- solutions/task2_8.py
+++ solutions/task2_8.py
@@ -3,12 +3,16 @@ a = [0] * n
 b = [0] * n
 c = [0] * n
 for i in range(n):
-    a[i], b[i], c[i] = map(int, input().split())
+  a[i], b[i], c[i] = map(int, input().split())
+  
+g = [-1] * n
+g[0] = a[0]
 
-ans = [0, a[0], min(a[0] + a[1], b[0])]
-
-for j in range(3, n + 1):
-    tmp = min(ans[j - 1] + a[j - 1], ans[j - 2] + b[j - 2])
-    ans.append(min(tmp, ans[j - 3] + c[j - 3]))
-
-print(ans[-1])
+if n >= 2:
+  g[1] = min(b[0], a[0] + a[1])
+if n >= 3:
+  g[2] = min(g[1] + a[2], g[0] + b[1], c[0], a[0] + a[1] + a[2])
+  for i in range(3, n):
+    g[i] = min(g[i - 1] + a[i], g[i - 2] + b[i - 1], g[i - 2] + a[i - 1] + a[i], g[i - 3] + c[i - 2], g[i - 3] + b[i - 2] + a[i], g[i - 3] + a[i - 2] + b[i - 1], g[i - 3] + a[i - 2] + a[i - 1] + a[i])
+    
+print(g[n - 1])
